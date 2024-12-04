df2062c73c2ebd7e6b2698d09a7737f2d72b22fc
Jozelle <51460429+Jozelle@users.noreply.github.com>
2024-12-04 14:41:00 +0100

    Finished day 2 :)

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,9 +1,9 @@
-﻿//string folderpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-//string filename = "input2.txt";
+﻿string folderpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+string filename = "input2.txt";
 
-//string filepath = Path.Combine(folderpath, filename);
+string filepath = Path.Combine(folderpath, filename);
 
-string filepath = "test2.txt";
+//string filepath = "test2.txt";
 
 while (true)
 {
@@ -21,7 +21,7 @@ while (true)
                 Part1(filepath);
                 break;
             case 2:
-                //Part2(leftList, rightList);
+                Part2(filepath);
                 break;
             default:
                 Console.WriteLine("Invalid input");
@@ -54,11 +54,35 @@ static void Part1(string filepath)
     }
 
     Console.WriteLine(safeCount);
+
+    // var input = File.ReadLines(filepath)
+    //.Select(s => s.Split(' ').Select(int.Parse));
+
+    // Console.WriteLine(input.Count(IsSafe1));
 }
 
-static void Part2()
+static void Part2(string filepath)
 {
+    List<string> testdata = GetTestdata(filepath);
 
+    int safeCount = 0;
+    foreach (var item in testdata)
+    {
+        int[] split = item.Split(' ').Select(int.Parse).ToArray();
+
+        if (IsLevelSafeDampened(split))
+        {
+            safeCount++;
+        }
+
+    }
+
+    Console.WriteLine(safeCount);
+
+    // var input = File.ReadLines(filepath)
+    //.Select(s => s.Split(' ').Select(int.Parse));
+
+    // Console.WriteLine(input.Count(IsSafe2));
 }
 
 static List<string> GetTestdata(string filepath)
@@ -86,14 +110,35 @@ static (bool, int) IsLevelSafe(int[] values)
 
     for (int i = 1; i < values.Length - 1; i++)
     {
-        var (increase, safeDifference) = CheckDifference(values[i], values[i + 1]);
-        if (!safeDifference ||increase != increasing)
+        var (increase, safeDiff) = CheckDifference(values[i], values[i + 1]);
+        if (!safeDiff ||increase != increasing)
         {
             return (false, i);
         }
     }
 
     return (true, -1);
+}
+
+static bool IsLevelSafeDampened(int[] values)
+{
+    var (isSafe, unsafeIndex) = IsLevelSafe(values);
+
+    if(isSafe)
+    {
+        return true;
+    }
+
+    for (var i = unsafeIndex > 0 ? unsafeIndex - 1 : unsafeIndex; i <= unsafeIndex + 1; i++)
+    {
+        (isSafe, _) = IsLevelSafe([.. values[..i], .. values[(i + 1)..]]);
+        if (isSafe)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 static (bool, bool) CheckDifference (int x, int y)
@@ -105,3 +150,15 @@ static (bool, bool) CheckDifference (int x, int y)
 
     return (isIncreasing, amount is 1 or 2 or 3);       // Return a tuple with the results, second value is a boolean that returns true if the amount is 1, 2 or 3
 }
+
+//Stylish solution from dmitry-shechtman/aoc2024 on GitHub
+static bool IsSafe1(IEnumerable<int> a) =>
+    IsSafe(a.Zip(a.Skip(1), (x, y) => x - y));
+
+static bool IsSafe2(IEnumerable<int> a) =>
+    Enumerable.Range(0, a.Count())
+        .Any(i => IsSafe1(a.Take(i).Concat(a.Skip(i + 1))));
+
+static bool IsSafe(IEnumerable<int> b) =>
+    b.All(x => x >= -3 && x <= -1) ||
+    b.All(x => x >= 1 && x <= 3);
